5bf13a4f8514da565992b997a24c604cb17866d0
zol-delek <ilcapro@gmail.com>
2026-03-04 19:47:03 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,16 +1,9 @@
-async function getFuelPrices() {
-  try {
-    // כאן בהמשך נשים את הקוד שמושך מאתרים אמיתיים
-    // כרגע זה מחזיר נתונים קבועים כדי לוודא שהאפליקציה עובדת
-    return [
-      { station: "דלק", price: "7.29", location: "פריסה ארצית" },
-      { station: "סונול", price: "7.25", location: "תל אביב" },
-      { station: "דור אלון", price: "7.21", location: "חיפה" }
-    ];
-  } catch (e) {
-    console.error("Scraper Error:", e);
-    return [];
-  }
-}
+const getFuelPrices = async () => {
+  return [
+    { "station": "דלק", "price": "7.29", "location": "פריסה ארצית" },
+    { "station": "סונול", "price": "7.25", "location": "תל אביב" },
+    { "station": "דור אלון", "price": "7.21", "location": "חיפה" }
+  ];
+};
 
 module.exports = { getFuelPrices };
