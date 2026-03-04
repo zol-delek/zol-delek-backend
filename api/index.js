ef0db5620a3aa0a36d3692579cd71be3012b3911
zol-delek <ilcapro@gmail.com>
2026-03-04 19:56:49 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
+
 const express = require('express');
 const app = express();
 const { getFuelPrices } = require('../scraper');
+
 app.get('/api/prices', async (req, res) => {
   try {
     const prices = await getFuelPrices();
     res.json(prices);
   } catch (error) {
-    res.status(500).json({ error: "failed" });
+    res.status(500).json({ error: "Server Error" });
   }
 });
+
 module.exports = app;
